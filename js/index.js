70343e00e71f08f674a108fd496471c23166b67c
German Gauna <germangauna88@gmail.com>
2022-08-24 21:13:47 -0300

    agregue js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// crear un algoritmo
+
+let contraseña = parseInt(prompt("ingrese contraseña"));
+
+if (contraseña === 5) {
+    alert("puedes ingresar");
+} else {
+    alert("ingresa nuevamente la contraseña");
+}
