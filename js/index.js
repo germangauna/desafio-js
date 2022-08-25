04674a3f236508c5adf7cb59b480bc3f3647414e
German Gauna <germangauna88@gmail.com>
2022-08-24 21:42:06 -0300

    cree algoritmo ciclo for

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 // crear un algoritmo
 
-let contraseña = parseInt(prompt("ingrese contraseña"));
+/* let contraseña = parseInt(prompt("ingrese contraseña"));
 
 if (contraseña === 5) {
     alert("puedes ingresar");
 } else {
     alert("ingresa nuevamente la contraseña");
+} */
+
+for (let i = 0; i < 50; i++){
+    let nombre = prompt("ingrese su nombre");
+    let mensaje = `entrada ${i} nombre: ${nombre}`;
+    alert(mensaje);
 }
+alert("ya se terminaron las entradas");
