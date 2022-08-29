f53bd14cc441e721b9f13227dd4b95f25c73a706
German Gauna <germangauna88@gmail.com>
2022-08-29 20:30:05 -0300

    trabajo practico crear un simulador

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
-// crear un algoritmo
+// trabajo practico simulador: convertidor de moneda
 
-/* let contraseña = parseInt(prompt("ingrese contraseña"));
+//en nuestro parseInt tenemos que ingresar la palabra valor, la mism que pusimos en el id del formulario del index.html, donde dice la cifra que podemos poner de 1 a 100000. la palabra valuees para verifique los valores que puse en el 
+    function convertir() {
+        let valor = parseInt(document.getElementById("valor").value);
+        let resultado = 0;
+        let dolar = 287;
+        let euro = 295;
+// en este caso pondremos la palabra "uno" que es la que pusimos en el id del index.html
+        if (document.getElementById("uno").checked) {
+            resultado = valor / dolar;
+            alert("el cambio de pesos a dolares es: $ " + resultado.toFixed[2]);//tofixed es para darle la cantidad de decimales
+        } else if (document.getElementById("dos").checked) {
+            resultado = valor / euro;
+            alert("el cambio de pesos a euro es: " + resultado.tofixed[2]);
 
-if (contraseña === 5) {
-    alert("puedes ingresar");
-} else {
-    alert("ingresa nuevamente la contraseña");
-} */
+        } else {
+            alert("tienes que completar los campor requeridos")
+        }
+    }
 
-for (let i = 0; i < 50; i++){
-    let nombre = prompt("ingrese su nombre");
-    let mensaje = `entrada ${i} nombre: ${nombre}`;
-    alert(mensaje);
-}
-alert("ya se terminaron las entradas");
+
+// llamamos la funcion desde el index.html
